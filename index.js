21d6ad66a8e629c68a85ea48e8b9a4f6380e23f6
Jonas Enge <jonas.enge@gmail.com>
2018-04-06 14:30:34 +0200

    Adds changes for SvarUt v9

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const wsSend = require('./lib')
 
-function setMottaker (mottaker) {
-  const type = mottaker.fodselsnr ? 'privatPerson' : 'organisasjon'
+function setPostAdresse (mottaker) {
+  delete mottaker.fnr
+  delete mottaker.orgnr
+  return mottaker
+}
 
+function setDigitalAdresse (mottaker) {
+  if (mottaker.fnr) {
+    return {
+      '_xsi:type': 'ns2:personDigitalAdresse',
+      '_xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+      fnr: mottaker.fnr
+    }
+  }
   return {
-    '_xsi:type': 'ns2:' + type,
+    '_xsi:type': 'ns2:organisasjonDigitalAdresse',
     '_xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
-    ...mottaker
+    orgnr: mottaker.orgnr
   }
 }
 
@@ -15,7 +26,10 @@ function setDokumenter (dokumenter) {
     return {
       data: '',
       filnavn: dokument.filnavn,
-      mimetype: dokument.mimetype
+      mimetype: dokument.mimetype,
+      skalSigneres: dokument.skalSigneres || false,
+      dokumentType: dokument.dokumentType || undefined,
+      ekskluderesFraPrint: dokument.ekskluderesFraPrint || false
     }
   })
 }
@@ -25,13 +39,27 @@ exports.sendForsendelse = options => {
   options._dokumenter = Object.assign([], options.query.dokumenter)
   options.query.dokumenter = setDokumenter(options.query.dokumenter)
   if (options.query.svarSendesTil) {
-    options.query.svarSendesTil = setMottaker(options.query.svarSendesTil)
+    options.query.svarSendesTil = {
+      digitalAdresse: setDigitalAdresse(options.query.svarSendesTil),
+      postAdresse: setPostAdresse(options.query.svarSendesTil)
+    }
   }
 
   async function createJobs (mottaker) {
-    let opts = Object.assign({}, options)
-    opts.query.mottaker = setMottaker(mottaker)
-    opts.query = { forsendelse: opts.query }
+    const opts = {
+      config: options.config,
+      action: options.action,
+      _dokumenter: options._dokumenter,
+      query: {
+        forsendelse: {
+          ...options.query,
+          mottaker: {
+            digitalAdresse: setDigitalAdresse(mottaker),
+            postAdresse: setPostAdresse(mottaker)
+          }
+        }
+      }
+    }
     return wsSend(opts)
   }
   const jobs = Array.isArray(options.query.mottaker) ? options.query.mottaker.map(createJobs) : [createJobs(options.query.mottaker)]
